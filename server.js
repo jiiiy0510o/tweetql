434e41a4fbec358d40d091d34cff9d5fb1e3a55f
Young <jiiiy0510o@gmail.com>
2022-06-12 18:49:55 +0900

    from REST to GraphQL

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import { ApolloServer, gql } from "apollo-server";
+import fetch from "node-fetch";
 
 let tweets = [
   {
@@ -22,7 +23,7 @@ let users = [
   {
     id: "2",
     firstName: "Eric",
-    lastName: "Moon",
+    lastName: "Yoon",
   },
 ];
 
@@ -31,21 +32,56 @@ const typeDefs = gql`
     id: ID!
     firstName: String!
     lastName: String!
+    """
+    Is the sum of first Name + lastName as a string
+    """
     fullName: String!
   }
+  """
+  Tweet object represents a resource for a tweet
+  """
   type Tweet {
     id: ID!
     text: String!
     author: User
   }
   type Query {
+    allMovies: [Movie!]!
     allUsers: [User!]!
     allTweets: [Tweet!]!
     tweet(id: ID!): Tweet
+    movie(id: String!): Movie
   }
   type Mutation {
     postTweet(text: String!, userId: ID!): Tweet!
+    """
+    Deletes a Tweet if found, else returns false
+    """
     deleteTweet(id: ID!): Boolean!
+  }
+  type Movie {
+    id: Int!
+    url: String!
+    imdb_code: String!
+    title: String!
+    title_english: String!
+    title_long: String!
+    slug: String!
+    year: Int!
+    rating: Float!
+    runtime: Float!
+    genres: [String!]!
+    summary: String
+    description_full: String!
+    synopsis: String
+    yt_trailer_code: String!
+    language: String!
+    mpa_rating: String!
+    background_image: String!
+    background_image_original: String!
+    small_cover_image: String!
+    medium_cover_image: String!
+    large_cover_image: String!
   }
 `;
 
@@ -59,6 +95,16 @@ const resolvers = {
     },
     allUsers() {
       return users;
+    },
+    allMovies() {
+      return fetch("https://yts.mx/api/v2/list_movies.json").then((response) =>
+        response.json().then((json) => json.data.movies)
+      );
+    },
+    movie(_, { id }) {
+      return fetch("https://yts.mx/api/v2/list_movies.json?movie_id=${id}").then((response) =>
+        response.json().then((json) => json.data.movie)
+      );
     },
   },
   Mutation: {
